7a29dc32a753285fe00c6c192b4cbeb5e6f964f4
apsillers <apsillers@5dd7d095-bbfe-d659-e918-d74cce746d7e>
2011-01-02 07:58:43 +0000

    Added games, altered add_to_library slightly, tweaked home.

--- trunk/init.js
+++ trunk/init.js
@@ -1,25 +1,31 @@
 // if this is a new install or the local storage has been wiped
 var ver = window.localStorage.getItem("storage_version");
-if(ver != "1") {
+if(ver == null) {
     // pull a couple games from the hard drive
 	var urldomain = /^(file:|([\w-]+:)?\/\/[^\/?#]+)/;
 	var page_domain = urldomain.exec(location)[0];
 
-    xmlhttp = new XMLHttpRequest();
-    var url = "stories/curses.z5";
+    var xmlhttp = new XMLHttpRequest();
+    var url;
+    var story_data;
     xmlhttp.overrideMimeType('text/plain; charset=x-user-defined');
-    xmlhttp.open("GET",url,false);
-    xmlhttp.send();
-    var story_data = file.text_to_array( xmlhttp.responseText );
-    b64_data = file.base64_encode(story_data);
-    file.add_to_library("http://ifarchive.org/if-archive/games/zcode/curses.z5", b64_data);
 
-    url = "stories/photopia.z5";
-    xmlhttp.open("GET",url,false);
-    xmlhttp.send();
-    var story_data = file.text_to_array( xmlhttp.responseText );
-    b64_data = file.base64_encode(story_data);
-    file.add_to_library("http://ifarchive.org/if-archive/games/zcode/photopia.z5", b64_data);
+    var preloaded_games = {
+        "stories/curses.z5":{"link":"http://ifarchive.org/if-archive/games/zcode/curses.z5", "title":"Curses!", "author":"Graham Nelson"},
+        "stories/metamorp.z5":{"link":"http://ifarchive.org/if-archive/games/zcode/metamorp.z5", "title":"Metamorphoses", "author":"Emily Short"},
+        "stories/photopia.z5":{"link":"http://ifarchive.org/if-archive/games/zcode/photopia.z5", "title":"Photopia", "author":"Adam Cadre"},
+        "stories/suvehnux.z5":{"link":"http://ifarchive.org/if-archive/games/zcode/suvehnux.z5", "title":"Suveh Nux", "author":"David Fisher"},
+        "stories/dreamhold.z8":{"link":"http://ifarchive.org/if-archive/games/zcode/dreamhold.z8", "title":"The Dreamhold", "author":"Andrew Plotkin"}
+    };
 
-    window.localStorage.setItem("init", true);
+    for(url in preloaded_games) {
+        var curr_game = preloaded_games[url];
+        xmlhttp.open("GET",url,false);
+        xmlhttp.send();
+        story_data = file.text_to_array( xmlhttp.responseText );
+        b64_data = file.base64_encode(story_data);
+        file.add_to_library(curr_game, b64_data, false);
+    }
+
+    window.localStorage.setItem("storage_version", 1);
 }
